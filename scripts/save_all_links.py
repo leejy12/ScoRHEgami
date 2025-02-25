fab92b40497c900458be69c90add30d9341177e0
Junyoung Lee <leejy31415@outlook.com>
2025-02-25 23:59:05 +0900

    add links to all games

---
+++ scripts/save_all_links.py
@@ -0,0 +1,19 @@
+import ScoRHEgami.baseball_reference as bref
+import time
+
+
+def main():
+    first_season = 1901
+    last_season = 2024
+    for season in range(first_season, last_season + 1):
+        urls = bref.get_links_of_season(season)
+        urls_text = "\n".join(urls)
+
+        with open(f"links/{season}.txt", "w") as f:
+            f.write(urls_text)
+
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
