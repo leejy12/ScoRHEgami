e812040c93bd6627e610516035ccf81b23c6b41e
Junyoung Lee <leejy31415@outlook.com>
2025-03-03 20:21:05 +0900

    Add game results (1901-1904)

---
+++ scripts/record_games.py
@@ -0,0 +1,27 @@
+import ScoRHEgami.baseball_reference as bref
+import time
+
+
+def main():
+    first_season = 1905
+    last_season = 2024
+    for season in range(first_season, last_season + 1):
+        with open(f"links/{season}.txt", "r") as f:
+            with open(f"results/{season}.txt", "a") as result_file:
+                urls = [url.strip() for url in f.readlines()]
+                for url in urls:
+                    while True:
+                        try:
+                            result = bref.get_game_result(url)
+                            result_file.write(f"{result.model_dump_json()}\n")
+                            print(f"SUCCESS: {url}")
+                            time.sleep(5)
+                            break
+                        except RuntimeError as e:
+                            print(e)
+                            time.sleep(5)
+                            continue
+
+
+if __name__ == "__main__":
+    main()
