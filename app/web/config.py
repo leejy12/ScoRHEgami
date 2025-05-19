4e4b1f8eadb25f364bd216e9fc6130b2ad059e69
Junyoung Lee <leejy31415@outlook.com>
2025-05-20 00:11:06 +0900

    delete html

--- app/web/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from fastapi.templating import Jinja2Templates
-
-base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-templates_dir = os.path.join(base_dir, "templates")
-
-templates = Jinja2Templates(directory=templates_dir)
